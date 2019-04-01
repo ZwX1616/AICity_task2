b7d168387784bbc634b920299d90f318ab5f8561
ZwX1616 <zwx1616@gmail.com>
2019-04-01 00:21:31 -0400

    add saving and logging

--- TF_implementation/train.py
+++ TF_implementation/train.py
@@ -6,8 +6,10 @@ from network import Siamese_classic_mobilenet
 from dataloader import DataLoader_train
 
 learning_rate = 0.1
-num_iter = 100000
+num_iter = 10000
 batch_size = 256
+
+save_interval = 25
 
 data_shape = (224,224,3)
 
@@ -36,10 +38,16 @@ my_dataloader = DataLoader_train(batch_size, data_shape)
 # variable saver
 saver = tf.train.Saver()
 
+# tensorboard logger
+writer_train = tf.summary.FileWriter('./logs/')
+tf.summary.scalar("net_loss", net.loss)
+merged_summary = tf.summary.merge_all()
+
+# iterate
 for epoch in range(num_iter):
 	start = time()
 	x1, x2, y = my_dataloader.get_batch()
-	_, l = sess.run([train_step, net.loss], feed_dict={
+	_, summary, l = sess.run([train_step, merged_summary, net.loss], feed_dict={
 						net.x1: x1,
 						net.x2: x2,
 						net.y_gt: y})
@@ -48,7 +56,16 @@ for epoch in range(num_iter):
 		print('Model diverged with loss = NaN')
 		quit()
 
+	# logging
 	print ('iteration %d: loss %.7f, runtime: %.1fs' % (epoch, l, time()-start))
+	if (epoch==0):
+		writer_train.add_graph(sess.graph)
+	writer_train.add_summary(summary, epoch)
+	writer_train.flush()
+	if (epoch>0 and epoch%save_interval==0):
+		saver.save(sess, './checkpoints/model')
+		print ('...checkpoint saved...')
 
-saver.save(sess, './model')
+print (str(num_iter)+' iterations completed')
+writer_train.close()
 sess.close()
